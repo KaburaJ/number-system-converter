8ffecbd95f2dc2f3e36f1eb6cab2857c842e52b8
Joan Kabura <kaburajoan25@gmail.com>
2023-06-14 06:32:19 -0700

    v 1.0

---
+++ src/App.js
@@ -0,0 +1,108 @@
+import './App.css';
+import React, { useState } from 'react';
+
+function App() {
+  const options = [
+    { value: '0b', label: 'Binary' },
+    { value: '10', label: 'Decimal' },
+    { value: '0o', label: 'Octal' },
+    { value: '0x', label: 'Hexadecimal' },
+  ];
+
+  const [value, setValue] = useState('');
+  const [inputSystem, setInputSystem] = useState(options[0]);
+  const [outputSystem, setOutputSystem] = useState(options[1]);
+  const [convertedValue, setConvertedValue] = useState('');
+
+  const handleNumberInput = (e) => {
+    setValue(e.target.value);
+    convertNumber(e.target.value);
+  };
+
+  const handleInputSystemChange = (e) => {
+    const selectedValue = e.target.value;
+    const selectedOption = options.find((option) => option.value === selectedValue);
+    setInputSystem(selectedOption);
+    convertNumber(value);
+  };
+
+  const handleOutputSystemChange = (e) => {
+    const selectedValue = e.target.value;
+    const selectedOption = options.find((option) => option.value === selectedValue);
+    setOutputSystem(selectedOption);
+    convertNumber(value);
+  };
+
+  const convertNumber = (num) => {
+    let converted = '';
+
+    if (/^[01]+$/.test(num)) {
+      if (outputSystem.value === '10') {
+        converted = parseInt(num, 2).toString(10);
+      } else if (outputSystem.value === '0o') {
+        converted = parseInt(num, 2).toString(8);
+      } else if (outputSystem.value === '0x') {
+        converted = parseInt(num, 2).toString(16);
+      }
+    } else if (/^\d+$/.test(num)) {
+      if (outputSystem.value === '0b') {
+        converted = parseInt(num, 10).toString(2);
+      } else if (outputSystem.value === '0o') {
+        converted = parseInt(num, 10).toString(8);
+      } else if (outputSystem.value === '0x') {
+        converted = parseInt(num, 10).toString(16);
+      }
+    } else if (/^[0-7]+$/.test(num.slice(2))) {
+      if (outputSystem.value === '0b') {
+        converted = parseInt(num, 8).toString(2);
+      } else if (outputSystem.value === '10') {
+        converted = parseInt(num, 8).toString(10);
+      } else if (outputSystem.value === '0x') {
+        converted = parseInt(num, 8).toString(16);
+      }
+    } else if (/^[0-9A-Fa-f]+$/.test(num.slice(2))) {
+      if (outputSystem.value === '0b') {
+        converted = parseInt(num, 16).toString(2);
+      } else if (outputSystem.value === '10') {
+        converted = parseInt(num, 16).toString(10);
+      } else if (outputSystem.value === '0o') {
+        converted = parseInt(num, 16).toString(8);
+      }
+    }
+
+    setConvertedValue(converted);
+  };
+
+  return (
+    <div className="App">
+      <div className='bothSelectors'>
+      <div>
+        <label>Input System:</label>
+        <select className="selector" value={inputSystem.value} onChange={handleInputSystemChange}>
+          {options.map((option) => (
+            <option key={option.value} value={option.value}>
+              {option.label}
+            </option>
+          ))}
+        </select>
+      </div>
+      <div>
+        <label>Output System:</label>
+        <select className="selector" value={outputSystem.value} onChange={handleOutputSystemChange}>
+          {options.map((option) => (
+            <option key={option.value} value={option.value}>
+              {option.label}
+            </option>
+          ))}
+        </select>
+      </div>
+      </div>
+      <input className="input" value={value} onChange={handleNumberInput} placeholder='Enter number for conversion'/>
+      <h2>
+        Converted Value: 
+        <value>{convertedValue}</value></h2>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/evaluation.js
@@ -0,0 +1,35 @@
+import React from 'react';
+
+const calculator = () =>{  
+
+    const decimalToBinary = (value)=>{
+        parseInt(value, 2)
+    }
+
+    const decimalToHex = (value) =>{
+        parseInt(value, 16)
+    }
+
+    const decimalToOctal = (value) =>{
+        parseInt(value, 8)
+    }
+
+    const binaryToDecimal = (value)=>{
+
+        // const binary(value){
+        //     while(value != null){
+        //         value=value/10
+        //     }
+           
+        // }
+    }
+
+    const binaryToHex = (value) =>{
+        parseInt(value, 16)
+    }
+
+    const binaryToOctal = (value) =>{
+        parseInt(value, 8)
+    }
+
+}
